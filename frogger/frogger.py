2bb4c6a544cf3d37c0d21a809d28dc452775a400
Yan <Yanholo@icloud.com>
2025-03-07 20:47:46 +0000

    cars

--- frogger/frogger.py
+++ frogger/frogger.py
@@ -1,9 +1,9 @@
-import pygame as py
+import pygame as py, random
 
 py.init()
 
 ## Window Settings ##
-WIDTH, HEIGHT = 801, 1000
+WIDTH, HEIGHT = 1101, 1100
 screen = py.display.set_mode((WIDTH, HEIGHT))
 py.display.set_caption("Frogger")
 
@@ -19,6 +19,9 @@ cars = [
     py.image.load("frogger/images/car3L.png"),
     py.image.load("frogger/images/car4.png")
 ]
+
+cars = [py.transform.scale(i, (100,50)) for i in cars]*2
+CD = random.randint(60,180)
 
 ## Sound ##
 py.mixer.music.load("frogger/music.mp3")
@@ -61,26 +64,55 @@ class Frog(py.sprite.Sprite):
         self.image = py.transform.rotate(self.image, self.angle)
         self.rect = self.image.get_rect(center=self.rect.center)
 
+class Car(py.sprite.Sprite):
+    def __init__(self, image, x, y, speed):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect(center=(x, y))
+        self.speed = speed
+
+    def update(self):
+        self.rect.x += self.speed
+
+        if self.rect.x < -120:
+            self.rect.x = WIDTH
+
 ## Road Class ##
 class Road(py.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.car_rect = [i.get_rect() for i in cars]
+        self.car_group = py.sprite.Group()
+        self.lanes = []
+    
 
-
+    ## temp ##
     def draw_road(self):
-        x, y = WIDTH, HEIGHT - 350
-        py.draw.rect(screen, GRAY, (0, y, x, 200))
-        py.draw.line(screen, LIGHT_GRAY, (0, y), (x, y), 5)
-        py.draw.line(screen, LIGHT_GRAY, (0, y + 200), (WIDTH, y + 200), 5)
-        
-        for i in range(0, WIDTH, 40):
-            py.draw.line(screen, WHITE, (i + 10, y + 100), (i + 30, y + 100), 5)
+        ## road ##
+        for i in range(8):
+            y = HEIGHT - (10 * TILE_SIZE) + (i * (TILE_SIZE))
+            py.draw.rect(screen, GRAY, (0, y, WIDTH, TILE_SIZE))
 
-    
-    
+            ## kerbs ##
+        py.draw.line(screen, LIGHT_GRAY, (0, 600), (WIDTH, 600), 6)
+        py.draw.line(screen, LIGHT_GRAY, (0, y + TILE_SIZE), (WIDTH, y + TILE_SIZE), 6)
+
+            ## dotted line ##
+        for j in range(0, WIDTH, TILE_SIZE):
+            py.draw.line(screen, WHITE, (j + 10, y + 2 * TILE_SIZE), (j + 40, y + 2 * TILE_SIZE), 4)
+
+    def add_car(self, image, x, y, speed):
+
+        car = Car(image, x, y, speed)
+        self.car_group.add(car)
+        
     def draw_cars(self):
-        pass
+        self.car_group.draw(screen)
+    
+    def update(self):
+        self.car_group.update()
+
+
+
 
 ## Temporary Grid ##
 def draw_grid():
@@ -89,9 +121,14 @@ def draw_grid():
     for row in range(ROWS + 1):
         py.draw.line(screen, WHITE, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
 
+
+
 frog = Frog()
 road = Road()
-sprites = py.sprite.Group(frog)
+sprite = py.sprite.Group(frog)
+car_sprite = py.sprite.Group()
+
+
 
 ## Game Loop ##
 running = True
@@ -103,13 +140,28 @@ while running:
             running = False
         if ev.type == py.KEYDOWN:
             frog.move_frog()
+
+    
     
     road.draw_road()
-    sprites.update()
-    sprites.draw(screen)
-    draw_grid()
+    sprite.update()
+    sprite.draw(screen)
+
     
+    road.update()
+    road.draw_cars()
+
+    if CD == 0:
+        x = random.randint(1,7)
+        road.add_car(cars[0],1200, 625 + (x*50), -5)
+        CD = random.randint(80,160)
+
+    CD -=1    
+    draw_grid()
+
+    
+    animation.tick(100)
     py.display.flip()
-    animation.tick(60)
+    
 
 py.quit()
